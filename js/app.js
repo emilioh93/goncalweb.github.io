740c6aadc616efba1501ac948b9340a74c60aee6
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-07-28 20:13:13 -0300

    cambiamos fondo de mobile

--- js/app.js
+++ js/app.js
@@ -1,19 +1,8 @@
-$(window).scroll(function() {
-  // Get scroll position
-  var s = $(window).scrollTop(),
-  // scroll value and opacity
-  opacityVal = (s / 150.0);
-  // opacity value 0% to 100%
-  $('.blurred-img').css('opacity', opacityVal);
-});
-
-/* -------------------------------- Parallax -------------------------------- */
-
-function parallax() {
-  window.onscroll = function() {
-    var speed = 5.0;
-    document.body.style.backgroundPosition = (-window.pageXOffset/speed)+"px "+(-window.pageYOffset/speed)+"px";
-  }
-}
-
-parallax();
+// $(window).scroll(function() {
+//   // Get scroll position
+//   var s = $(window).scrollTop(),
+//   // scroll value and opacity
+//   opacityVal = (s / 150.0);
+//   // opacity value 0% to 100%
+//   $('.blurred-img').css('opacity', opacityVal);
+// });
