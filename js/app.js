61a6245f5293a1ee72755ba9308ce9cb1d66ed42
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-12-11 19:07:42 -0300

    Se agregaron más enlaces y de forma dinámica. También se cambió background

--- js/app.js
+++ js/app.js
@@ -1,8 +1,55 @@
-// $(window).scroll(function() {
-//   // Get scroll position
-//   var s = $(window).scrollTop(),
-//   // scroll value and opacity
-//   opacityVal = (s / 150.0);
-//   // opacity value 0% to 100%
-//   $('.blurred-img').css('opacity', opacityVal);
-// });
+const datos = [
+  {
+    enlace: "https://open.spotify.com/artist/0AgV5f8reGCw563caRiW6x",
+    icono: "spotify",
+    nombre: "Spotify",
+  },
+  {
+    enlace: "https://www.youtube.com/c/chukahurtado",
+    icono: "youtube",
+    nombre: "YouTube",
+  },
+  {
+    enlace: "https://music.youtube.com/channel/UC99ZJsmerzTwFw5-3sA1qMw",
+    icono: "youtube-square",
+    nombre: "YouTube Music",
+  },
+  {
+    enlace: "https://music.apple.com/co/artist/chuka-hurtado/1455257992",
+    icono: "apple",
+    nombre: "Apple Music",
+  },
+  {
+    enlace: "https://music.amazon.com/artists/B07PDJX36J/chuka-hurtado",
+    icono: "amazon",
+    nombre: "Amazon Music",
+  },
+  {
+    enlace: "https://www.deezer.com/us/artist/61012442",
+    icono: "deezer",
+    nombre: "Deezer",
+  },
+  {
+    enlace: "https://www.instagram.com/chukahurtado/",
+    icono: "instagram",
+    nombre: "Instagram",
+  },
+];
+
+datos?.map(
+  (dato) =>
+    (document.getElementById("LinkTree").innerHTML += `
+      <div class="contenedorLinkTree">
+        <a class="enlaceLinkTree" target="_blank" href=${dato.enlace}>
+            <div class="botonLinkTree">
+                <div class="col-1">
+                    <i class="fab fa-${dato.icono} fa-2x"></i>
+                </div>
+                <div class="col-11 text-center">
+                    <span class="nombreLinkTree">${dato.nombre}</span>
+                </div>
+            </div>
+        </a>
+      </div>
+    `)
+);
